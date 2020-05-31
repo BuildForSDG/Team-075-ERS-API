59732f58fdc8e1c2ed7ac48a8592905a05c4c15c
Jason Ibisi <j.ibisi@nestoilgroup.com>
2020-05-31 17:01:09 +0100

    feat: write and read responseUnit gps coordinates to json file
    This allows the gps locations of responseUnits to be read quickly for geolocation services
    
    Fixes #21

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { reportValidation } = require('../middleware/validationMiddleware');
+const { reportValidation, responseUnitLocationValidation } = require('../middleware/validationMiddleware');
 const auth = require('../middleware/authMiddleware');
 const reportCtrl = require('../controllers/reportController');
 
 const router = express.Router();
 
-router.post('/', auth, reportValidation, reportCtrl.createReport);
 router.get('/', auth, reportCtrl.getReports);
+router.post('/', auth, reportValidation, reportCtrl.createReport);
+router.post('/eru-location', responseUnitLocationValidation, reportCtrl.storeResponseUnitLocation);
+router.get('/eru-location', reportCtrl.getResponseUnitsLocation);
 
 module.exports = router;
