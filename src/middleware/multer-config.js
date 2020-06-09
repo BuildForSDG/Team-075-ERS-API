db94d165515611a049648b2451e21d9df5d693c1
Jason Ibisi <j.ibisi@nestoilgroup.com>
2020-06-09 16:01:59 +0100

    feat: report as eye witness
    Allows a user to request for help as an eye witness to an accident
    
    Fixes #33

---
+++ src/middleware/multer-config.js
@@ -0,0 +1,33 @@
+const multer = require('multer');
+// const fs = require('fs');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const DIR = './images/';
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const isValid = MIME_TYPES[file.mimetype];
+    let error = new Error('Invalid mime type');
+
+    if (isValid) {
+      error = null;
+    }
+    callback(error, DIR);
+  },
+  filename: (req, file, callback) => {
+    const name = file.fieldname
+      .toLowerCase()
+      .split(' ')
+      .join('_');
+    const extension = MIME_TYPES[file.mimetype];
+
+    callback(null, `${name + Date.now()}.${extension}`);
+  }
+});
+
+module.exports = multer({ storage }).single('image');
--- src/routes/report.js
+++ src/routes/report.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { auth } = require('../middleware/authMiddleware');
 const { reportValidation } = require('../middleware/validationMiddleware');
 const reportCtrl = require('../controllers/reportController');
+const multer = require('../middleware/multer-config');
 
 const router = express.Router();
 
 router.get('/', auth('admin'), reportCtrl.getReports);
 router.get('/id/:id', auth(['admin', 'user']), reportCtrl.getReport);
-router.post('/id/:id', auth(['admin', 'user']), reportValidation, reportCtrl.modifyReport);
+router.put('/id/:id', auth(['admin', 'user']), reportValidation, multer, reportCtrl.modifyReport);
 router.post('/', auth(['admin', 'user']), reportValidation, reportCtrl.createReport);
+router.post('/eye-witness', auth(['admin', 'user']), multer, reportCtrl.createReportAsWitness);
 
 module.exports = router;
