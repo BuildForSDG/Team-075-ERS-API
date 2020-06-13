1e48e91acc08e58319966c9a741b4bf9f7f294b6
Jason Ibisi <j.ibisi@nestoilgroup.com>
2020-06-13 17:27:34 +0100

    refactor: service worker

--- src/models/subscriber.js
+++ src/models/subscriber.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const subscriberSchema = mongoose.Schema({
   endpoint: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-  keys: { type: mongoose.Schema.Types.Mixed },
-  subscribedAt: { type: Date, default: Date.now }
+  keys: { type: mongoose.Schema.Types.Mixed }
 },
 { timestamps: true });
 
