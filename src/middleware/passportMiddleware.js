6e1df0bff2fb974d875bc4604c3ca6540b77a0c6
Jason Ibisi <j.ibisi@nestoilgroup.com>
2020-06-18 14:12:02 +0100

    fix: cors issue when logging in with socials
    Fixes #39

--- src/middleware/passportMiddleware.js
+++ src/middleware/passportMiddleware.js
@@ -54,6 +54,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL
+  // passReqToCallback: true
 }, (accessToken, refreshToken, profile, done) => {
   const {
     id, displayName, emails, provider
